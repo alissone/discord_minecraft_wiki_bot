c8fb312ae9a36b9288b9282b81269b2a67d4849b
alissone <alissond@acad.ifma.edu.br>
2020-04-04 17:10:36 -0300

    Now uses entire phrase as query, instead of individual words

--- TestingChatbot.py
+++ TestingChatbot.py
@@ -1,13 +1,9 @@
 from RecipeSearch import get_crafting_image
 from SynonymsTools import check_multi_syns_inside
-import inflect
 from SecretToken import token
-
 import discord
 
 TOKEN = token()
-
-import discord
 
 client = discord.Client()
 
@@ -16,19 +12,15 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 def craft_intent(message):
-    intent_words = ['craft', 'build', 'make', 'create']
+    intent_words = ['craft', 'build', 'make', 'create', 'recipe']
     return check_multi_syns_inside(intent_words, message.content)
 
 def craft_recipe_search(message):
-    inflect_engine = inflect.engine()
 
-    for word in message.content.split(' '):
-        crafting_recipe = get_crafting_image(word)
-        if not crafting_recipe:
-            plural_word = inflect_engine.plural(word)
-            crafting_recipe = get_crafting_image(plural_word)
-        if crafting_recipe:
-            return crafting_recipe
+    crafting_recipe = get_crafting_image(message.content)
+    print("crafting_recipe",crafting_recipe)
+    if crafting_recipe:
+        return crafting_recipe
 
 
 @client.event
